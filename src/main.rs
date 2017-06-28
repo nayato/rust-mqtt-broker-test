6e7cd1b4c2597335a692372482f4c09d03d7e897
Max Gortman <mgortman@microsoft.com>
2017-06-28 10:56:45 -0700

    perf?

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ extern crate tokio_tls;
 mod codec;
 
 use std::io;
-use mqtt::{Packet, ConnectReturnCode, QoS, SubscribeReturnCode, WritePacketExt};
+use mqtt::{Packet, ConnectReturnCode, QoS, SubscribeReturnCode, Codec, WritePacketExt};
 use tokio_proto::TcpServer;
 use tokio_proto::pipeline::ServerProto;
 use tokio_io::codec::{Decoder, Encoder, Framed};
@@ -37,7 +37,7 @@ use std::io::{Read, BufReader};
 use std::fs::File;
 
 #[derive(Default)]
-pub struct ProtoMqttCodec(codec::MqttCodec);
+pub struct ProtoMqttCodec(Codec);
 
 impl Decoder for ProtoMqttCodec {
     type Item = Option<Packet>;
@@ -70,7 +70,7 @@ impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for MqttProto {
     type BindTransport = Result<Self::Transport, io::Error>;
 
     fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(ProtoMqttCodec(codec::MqttCodec)))
+        Ok(io.framed(ProtoMqttCodec(Codec)))
     }
 }
 
@@ -91,18 +91,12 @@ impl Service for DummyService {
     type Future = BoxFuture<Self::Response, Self::Error>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        //println!("{:?}", req);
         let res = match req.expect("Can never be None") {
             Packet::Connect { .. } => Some(Packet::ConnectAck {
                 session_present: false,
                 return_code: ConnectReturnCode::ConnectionAccepted,
             }),
-            Packet::Publish {
-                qos,
-                packet_id: Some(pid),
-                ref payload,
-                ..
-            } if qos == QoS::AtLeastOnce => Some(Packet::PublishAck { packet_id: pid }),
+            Packet::Publish { qos, packet_id: Some(pid), ref payload, ..  } if qos == QoS::AtLeastOnce => Some(Packet::PublishAck { packet_id: pid }),
             Packet::Publish { qos, ref payload, .. } if qos == QoS::AtMostOnce => {
                 Some(Packet::Publish {
                     dup: false,
@@ -137,6 +131,7 @@ fn main() {
 }
 
 fn run() -> std::result::Result<(), std::io::Error> {
+    println!("{}", std::mem::size_of::<Packet>());
     let addr = "0.0.0.0:8113".parse().unwrap();
     // serve(addr, /*num_cpus::get()*/ 1, |h| MqttProto);
     let server = TcpServer::new(MqttProto, addr);
@@ -170,7 +165,7 @@ fn run() -> std::result::Result<(), std::io::Error> {
 
 impl MqttProto {
     pub fn bind<Io: AsyncRead + AsyncWrite + 'static>(&self, handle: &Handle, socket: Io) {
-        let framed = socket.framed(codec::MqttCodec);
+        let framed = socket.framed(Codec);
         let (tx, rx) = framed.split();
         let rex = rx.filter_map(move |req| match req {
             Packet::Connect { .. } => {
